0264eac0c31b26abf9875c8e3f794f3bf331467a
AayushChitransh11 <aayush11.ac@gmail.com>
2025-01-27 01:33:12 -0500

    updated config file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express=require('express');
+const cors = require('cors');
+
 
 
 const app=express();
 app.use(express.json());
+app.use(cors());
 
 const { userRouter }=require("./routes/user");
 const { courseRouter }=require("./routes/course");
